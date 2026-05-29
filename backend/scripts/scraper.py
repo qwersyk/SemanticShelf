6a9df88292368fb05b4c0f897fb79292f21e70f1
george <Heorhii.Shoilytsia@htlstp.at>
2026-05-29 08:26:18 +0200

    WIP

--- backend/scripts/scraper.py
+++ backend/scripts/scraper.py
@@ -1,8 +1,9 @@
-import json
-import re
-import webcolors
+
 import requests
 from bs4 import BeautifulSoup
+from threading import Thread
+from db import add_book, link_authors
+import time
 books = []
 
 def find_row(string , soup):
@@ -11,80 +12,84 @@ def find_row(string , soup):
     except:
         return None
 
-def wiki_row(soup):
-    try:
-        return soup.find("td", class_="label", string="Verfasser").find_parent("tr").find("td", class_="data").find_all("a")[
-            1].get("href")
-    except:
-        return None
+def scrape(a , b):
 
+    for i in range(a ,b+1):
+        try:
+            url = f'https://htl-stp.bibbs.cc/search?mode=stb&q=&critCount=3&crit_0=&op_0=&crit_1=&op_1=AND&ma=0&exAnz=0&flt=Alle&gradeFlt=&sort_0=Systematik&sort_1=Haupteintrag&sort_2=Haupttitel&page=1&view=detail&page_size=10&id=0.{i}'
 
+            response = requests.get(url, timeout=10)
 
+            print(f"request done {i}")
+            response.encoding = "utf-8"
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-def scrape():
-    for i in range(1 ,8000):
-        url = f'https://htl-stp.bibbs.cc/search?mode=stb&q=&critCount=3&crit_0=&op_0=&crit_1=&op_1=AND&ma=0&exAnz=0&flt=Alle&gradeFlt=&sort_0=Systematik&sort_1=Haupteintrag&sort_2=Haupttitel&page=1&view=detail&page_size=10&id=0.{i}'
-        response = requests.get(url)
-        response.encoding = "utf-8"
-        soup = BeautifulSoup(response.text, 'html.parser')
+            div = soup.select_one("div.detailData")
+            if not div:
+                continue
+            div_cover_image = soup.select_one("div.coverimage")
+            title = div.select_one("h3").text
+            if not title:
+                continue
+            isbn_raw = find_row("ISBN", soup)
+            isbn = "".join(d for d in isbn_raw if d.isdigit()) if isbn_raw else None
+            verfasser_row = find_row("Verfasser", soup)
+            verfasser = verfasser_row.split(";") if verfasser_row else None
+            verlag = find_row("Verlag", soup)
+            jahr = find_row("Jahr", soup)
+            umfang_row = find_row("Umfang", soup)
+            umfang = "".join(d for d in umfang_row if d.isdigit()) if umfang_row else None
+            sprache = find_row("Sprache", soup)
+            coverUrl = div_cover_image.select_one("img").attrs["src"]
+            book = {
+                "id" : i,
+                "title": title,
+                "isbn13": isbn,
+                "description": None,
+                "author": verfasser,
+                "publisher": verlag,
+                "year": jahr,
+                "pages": umfang,
+                "language": sprache,
+                "coverurl": coverUrl,
 
-        div = soup.select_one("div.detailData")
-        title = div.select_one("h3").text
-        if not title:
+            }
+
+            book_data = book.copy()
+
+            book_data.pop("author")
+            add_book(**book_data)
+            link_authors(book['id'] , book['author'])
+            print(f"Book {book['title']} was scraped")
+        except Exception as e:
+            print(f"Error on id {i}: {e}")
+
             continue
-        div_2 = soup.select_one("div.signaturetikett").find_all("div")
-        ort = find_row("Ort", soup)
-        abkuerzung = div_2[0].text
-        if div_2[1].get("style")[18:25]:
-            try:
-                color = webcolors.hex_to_name(div_2[1].get("style")[18:25])
-            except:
-                color = None
+
+
+
+def worker(amount, threads):
+    part = amount // threads
+    first = 1
+    threads_array = []
+
+    for i in range(1, threads + 1):
+        if i == threads:
+            end = amount
         else:
-            color = None
-        medienart = find_row("Medienart", soup)
-        isbn = find_row("ISBN", soup)
-        verfasser = find_row("Verfasser", soup)
-        verfasserWiki = wiki_row(soup)
-        verlag = find_row("Verlag", soup)
-        jahr = find_row("Jahr", soup)
-        umfang = find_row("Umfang", soup)
-        sprache = find_row("Sprache", soup)
-        verfasserangabe = find_row("Verfasserangabe", soup)
-        annotation = find_row("Annotation", soup)
-        book = {
-            "id" :f"0.{i}",
-            "title": title,
-            "medienart": medienart,
-            "isbn": isbn,
-            "author": verfasser,
-            "ort": ort,
-            "abkuerzung": abkuerzung,
-            "color": color,
-            "verfasserWiki": verfasserWiki,
-            "verlag": verlag,
-            "jahr": jahr,
-            "umfang": umfang,
-            "sprache": sprache,
-            "verfasserangabe": verfasserangabe,
-            "annotation": annotation,
+            end = first + part - 1
 
-        }
+        t = Thread(target=scrape, args=(first, end))
+        t.start()
+        threads_array.append(t)
 
+        first = end + 1
+        time.sleep(5)
 
-        books.append(book)
-        print(f"Book {book['title']} was scraped")
-    print(books)
-    with open("books.json", "w", encoding="utf-8") as file:
+    for t in threads_array:
+        t.join()
 
-        json.dump(books, file, ensure_ascii=False, indent=4)
-
-
-
-
-def clean_str(string):
-    return re.sub(r'\d+', '', string)
 
 
 if __name__ == '__main__':
-    scrape()
+    worker(8000 , 8)
