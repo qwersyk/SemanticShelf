cd772f8d4f2af4297534ea473544a203b67dc921
george <Heorhii.Shoilytsia@htlstp.at>
2026-06-10 14:58:05 +0200

    search button

--- frontend/semanticshelf/src/features/preview-book/preview-book.ts
+++ frontend/semanticshelf/src/features/preview-book/preview-book.ts
@@ -1,14 +1,14 @@
 import {Component, input} from '@angular/core';
 import {PreviewBook} from '../../core/models/preview-book';
 import { BookCover } from '../../shared/book-cover/book-cover';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-preview-book',
-  imports: [BookCover],
+  imports: [BookCover, RouterLink],
   templateUrl: './preview-book.html',
   styleUrl: './preview-book.scss',
 })
 export class PreviewBookComponent {
   readonly book = input.required<PreviewBook>();
-
 }
