71e83bba4bc34c61fb86837d18b1eee5fe070a11
george <Heorhii.Shoilytsia@htlstp.at>
2026-06-16 16:40:22 +0200

    404 page

--- frontend/semanticshelf/src/app/app.routes.ts
+++ frontend/semanticshelf/src/app/app.routes.ts
@@ -5,5 +5,7 @@ export const routes: Routes = [
   {path:"search" , loadComponent: () => import('../features/catalog/catalog').then(m => m.Catalog),} ,
   {path:"book/:id" , loadComponent: () => import("../features/book-site/book-site").then(m => m.BookSite),},
   {path:"search/:q" , loadComponent: () => import('../features/catalog/catalog').then(m => m.Catalog),},
+  {path:"404" , loadComponent: () => import('../features/not-found/not-found').then(m => m.NotFound),},
+
   {path:"**" ,redirectTo: ""}
 ];
