06c46f4b0181715d9b78ba6faa01d31205eb2722
george <Heorhii.Shoilytsia@htlstp.at>
2026-06-17 09:22:21 +0200

    404 page

--- frontend/semanticshelf/src/app/app.routes.ts
+++ frontend/semanticshelf/src/app/app.routes.ts
@@ -6,6 +6,5 @@ export const routes: Routes = [
   {path:"book/:id" , loadComponent: () => import("../features/book-site/book-site").then(m => m.BookSite),},
   {path:"search/:q" , loadComponent: () => import('../features/catalog/catalog').then(m => m.Catalog),},
   {path:"404" , loadComponent: () => import('../features/not-found/not-found').then(m => m.NotFound),},
-
-  {path:"**" ,redirectTo: ""}
+  {path:"**" ,redirectTo: "404"}
 ];
--- frontend/semanticshelf/src/features/book-site/book-site.ts
+++ frontend/semanticshelf/src/features/book-site/book-site.ts
@@ -47,7 +47,7 @@ export class BookSite {
             this.isLoading.set(false);
           },
           error: (err) => {
-            this.error.set('Failure during loading the book');
+            this.router.navigate(['/404']);
           },
         });
       }
